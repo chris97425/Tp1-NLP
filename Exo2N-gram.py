d12a0c996d2e65cdebcd285547177f14f1412cfc
christopher <christopher.deboisvilliers@gmail.com>
2017-10-12 17:52:18 -0400

    Import codecs pour ouverture en UTF-8

--- Exo2N-gram.py
+++ Exo2N-gram.py
@@ -1,7 +1,7 @@
 import os , copy, codecs
-
 os.chdir("detect_langue/")
 
+#Ici je créer un dictionnaire vide contenant l'ensemble des lettres du corpus étudié sans doublon
 def empty_dico(fich_txt):
 
     corpus={}
@@ -19,7 +19,9 @@ def empty_dico(fich_txt):
 #print(empty_dico("corpus_entrainement/portuguese-training.txt"))
 ##################################################################unigramme####################
 
-def uni_gr(fich_txt,boolres):
+#Ici je vais créer une fonction qui vas permettre de construire mon unigramme, boolres en fonction de sa valeur va renvoyer le compte des lettres
+#ou alors leurs probabilités, fich_txt sera le fichier texte d'entraînenemnt
+def uni_gr(fich_txt,boolres,test):
 
     corpus_empty_count = empty_dico(fich_txt)
 
@@ -34,13 +36,13 @@ def uni_gr(fich_txt,boolres):
             i+=1
             corpus_empty_count[ch]+=1
 
-    #print(i)
     corpus_empty_prob=copy.deepcopy(corpus_empty_count)
 
     for key in corpus_empty_prob:
 
         corpus_empty_prob[key]=corpus_empty_prob[key]/i
-
+    if (test == True):
+        corpus_empty_count["<UNK>"]=0
     if (boolres == False):
 
         return corpus_empty_count
@@ -49,14 +51,16 @@ def uni_gr(fich_txt,boolres):
 
         return corpus_empty_prob
 
-#print(uni_gr("corpus_entrainement/portuguese-training.txt",False))
+print(len(uni_gr("corpus_entrainement/portuguese-training.txt",False,True)))
 #print(uni_gr("english-training.txt",True))
 
 
-####################################################bigrammamammammammmammmammmammmammma##################
-def bi_grmo(fich_txt,boolret,test,fich_txt_test):
+#Ici je vais créer une fonction qui vas me permettre de construire un bigramme, fich_txt est notre fichier txt d'entraînement,
+#boolret est un booléen qui nous renvoie le bigramme sous forme de probabilité si False ou alors nous renvoie le bigramme sous forme de compte
+#test et fich_txt_test sont dans l'ordre un booléen pour préciser que l'on est dans la construction d'un bigramme en ajoutant
+def bi_grmo(fich_txt,boolret,test):
 
-    uni=uni_gr(fich_txt,False)
+    uni=uni_gr(fich_txt,False,False)
     textdoc = codecs.open(fich_txt, 'r', 'utf-8').read()
     emptyd=empty_dico(fich_txt)
     bigr_prob=copy.deepcopy(emptyd)
@@ -64,11 +68,6 @@ def bi_grmo(fich_txt,boolret,test,fich_txt_test):
     for key in bigr_prob:
         bigr_prob[key]={}
         bigr_count[key]={}
-
-    if (test == True):
-
-        ukn = word_unknown(fich_txt,fich_txt_test)["<UKN>"]
-
 
 
     for key in bigr_prob:
@@ -81,6 +80,20 @@ def bi_grmo(fich_txt,boolret,test,fich_txt_test):
         bigr_prob[textdoc[i]][textdoc[i+1]]+=1/uni[textdoc[i]]
         bigr_count[textdoc[i]][textdoc[i+1]]+=1
 
+    if (test == True):
+
+        for key in bigr_prob:
+            bigr_prob[key]["<UNK>"] = 0
+            bigr_count[key]["<UNK>"] = 0
+
+        bigr_prob["<UNK>"] = empty_dico(fich_txt)
+        bigr_prob["<UNK>"]["<UNK>"]=0
+
+        bigr_count["<UNK>"] = empty_dico(fich_txt)
+        bigr_count["<UNK>"]["<UNK>"] = 0
+
+
+
     if(boolret==False):
 
         return bigr_prob
@@ -88,14 +101,17 @@ def bi_grmo(fich_txt,boolret,test,fich_txt_test):
     else:
 
         return bigr_count
-#print(bi_grmo("corpus_entrainement/portuguese-training.txt",True))
+#print(bi_grmo("corpus_entrainement/english-training.txt",False,True))
 #################################################################trigramme ##################################
 
+#Ici je vais créer une fonction qui vas me permettre de construire un trigramme, fich_txt est notre fichier txt d'entraînement,
+#boolret est un booléen qui nous renvoie le trigramme sous forme de probabilité si False ou alors nous renvoie le trigramme sous forme de compte
+#test et fich_txt_test sont dans l'ordre un booléen pour préciser que l'on est dans la construction d'un bigramme en ajoutant
 def tri_grmo(fich_txt,boolre):
 
 
     textdoc = codecs.open(fich_txt, 'r', 'utf-8').read()
-    pro_bi_count = bi_grmo(fich_txt,True)
+    pro_bi_count = bi_grmo(fich_txt,True,False)
     dico_tri_gr = {}
 
     for i in range(0, len(textdoc)-2):
@@ -124,17 +140,21 @@ def tri_grmo(fich_txt,boolre):
 
 
 
-#print(tri_grmo("english-training.txt",False)["A "]["c"])
-
+#print(tri_grmo("corpus_entrainement/english-training.txt",False))
+#lissage de Laplace qui prend en entrée un n-gram un fichier text d'entraînement, boolres nous permet de renvoyer les nouvelles probabilités si True ou alors les nouveaux
+#compte si False, delta et le coefficient multiplicateur du lissage de Laplace
 def lissage_laplace(gram,fich_txt,boolres,delta):
 
     v_corpus=len(empty_dico(fich_txt))
+
+    print(v_corpus)
     for key in gram:
         break
 
     if (len(key)==1):
 
-        count_char = uni_gr(fich_txt,False)
+        count_char = uni_gr(fich_txt,False,True)
+
 
         for key in gram:
 
@@ -142,11 +162,12 @@ def lissage_laplace(gram,fich_txt,boolres,delta):
 
                 if (boolres==True):
 
-                    gram[key][key2] = (gram[key][key2]+(1*delta))/(count_char[key]+(v_corpus*delta))
+                    gram[key][key2] = (gram[key][key2]+(1*delta))/(count_char[key]+(len(gram)*delta))
+                    print(key,"et",key2," ont comme proba",gram[key][key2],'et la len(gram)',len(gram))
 
                 else:
 
-                    gram[key][key2] = ((gram[key][key2]+(1*delta)) * count_char[key]) / (count_char[key] + (v_corpus*delta))
+                    gram[key][key2] = ((gram[key][key2]+(1*delta)) * count_char[key]) / (count_char[key] + (len(gram)*delta))
 
     if (len(key)==2):
 
@@ -165,9 +186,9 @@ def lissage_laplace(gram,fich_txt,boolres,delta):
                     gram[key][key2] = ((gram[key][key2]+(1*delta)) * count_char2[key[0]][key[1]]) / (count_char2[key[0]][key[1]] + (v_corpus*delta))
 
     return gram
-#print(lissage_laplace(bi_grmo("english-training.txt",True),"english-training.txt",True))
+print(lissage_laplace(bi_grmo("corpus_entrainement/english-training.txt",True,True),"corpus_entrainement/english-training.txt",True,1))
 
-
+#Fonction d'interpolation linaire qui vas prendre en entré un n-Gram et un fichier test et nous renvoie un nouvelle unigramme lisser par interpolation linaire
 def interpola_linear(gram,fich_txt):
 
     uni=uni_gr(fich_txt,True)
@@ -201,7 +222,7 @@ def interpola_linear(gram,fich_txt):
 #print(interpola_linear(bi_grmo("english-training.txt",False),"english-training.txt"))
 #print("\n\n\n\n\n\n <>----------------------------<> \n\n\n\n\n\n")
 
-
+#Fonction qui vas me permettre de savoir quelle sont le nombre de lettre differente entre les deux fichier
 def word_unknown(fich_tex_ent,fich_text_test):
 
     unigram_ent=copy.deepcopy(uni_gr(fich_tex_ent,False))
@@ -217,7 +238,7 @@ def word_unknown(fich_tex_ent,fich_text_test):
             unigram_ent["<UNK>"] +=   unigram_test[key]
     return unigram_ent
 
-print(word_unknown("corpus_entrainement/english-training.txt","corpus_test/test20.txt")["<UNK>"])
+#print(word_unknown("corpus_entrainement/english-training.txt","corpus_test/test20.txt")["<UNK>"])
 
 
 def perplexity(laporint,fich_txt_test,gram_ent):
